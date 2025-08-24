5d4ef7a371646d3192ea08baf9d3cb4acbe54309
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-08-25 01:23:08 +0600

    Features Toggle Button

--- home.js
+++ home.js
@@ -29,6 +29,16 @@ function setInnerText(value){
     availableBalanceElement.innerText = value
 }
 
+// function to toggle
+function handleToggle(id){
+    const forms = document.getElementsByClassName("form")
+
+    for(const form of forms){
+        form.style.display = "none"
+    }
+    document.getElementById(id).style.display = "block"
+}
+
 // add money feature
 
 document.getElementById("add-money-btn").addEventListener("click", function(e){
@@ -83,10 +93,56 @@ setInnerText(totalNewAvailableBalance)
 //  toggling feature
 
 document.getElementById("add-button").addEventListener("click", function(){
-    document.getElementById("cash-out-parent").style.display = "none"
-    document.getElementById("add-money-parent").style.display = "block"
+
+    handleToggle("add-money-parent")
+    // const forms = document.getElementsByClassName("form")
+
+    // for(const form of forms){
+    //     form.style.display = "none"
+    // }
+
+    // document.getElementById("add-money-parent").style.display = "block"
+
+    // document.getElementById("add-money-parent").style.display = "block"
+    // document.getElementById("cash-out-parent").style.display = "none"
+    // document.getElementById("transfer-money-parent").style.display = "none"
 })
 document.getElementById("cashout-button").addEventListener("click", function(){
-    document.getElementById("add-money-parent").style.display = "none"
-    document.getElementById("cash-out-parent").style.display = "block"
+    // const forms = document.getElementsByClassName("form")
+
+    // for(const form of forms){
+    //     form.style.display = "none"
+    // }
+
+    // document.getElementById("cash-out-parent").style.display = "block"
+    handleToggle("cash-out-parent")
+
+    // document.getElementById("cash-out-parent").style.display = "block"
+    // document.getElementById("add-money-parent").style.display = "none"
+    // document.getElementById("transfer-money-parent").style.display = "none"
+})
+document.getElementById("transfer-button").addEventListener("click", function(){
+    handleToggle("transfer-money-parent")
+    // const forms = document.getElementsByClassName("form")
+
+    // for(const form of forms){
+    //     form.style.display = "none"
+    // }
+
+    // document.getElementById("transfer-money-parent").style.display = "block"
+
+    // document.getElementById("add-money-parent").style.display = "none"
+    // document.getElementById("cash-out-parent").style.display = "none"
+    // document.getElementById("transfer-money-parent").style.display = "block"
+})
+document.getElementById("bonus-button").addEventListener("click", function(){
+    handleToggle("get-bonus-parent")
+    // const forms = document.getElementsByClassName("form")
+
+    // for(const form of forms){
+    //     form.style.display = "none"
+    // }
+
+    // document.getElementById("get-bonus-parent").style.display = "block"
+
 })
