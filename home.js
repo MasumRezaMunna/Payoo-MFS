1b9f3f3716eefa6ff27cc3821271a74a24e966e3
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-08-24 11:18:15 +0600

    Implement Cashout Feature

--- home.js
+++ home.js
@@ -1,3 +1,4 @@
+// add money feature
 const validPin = 1234
 
 document.getElementById("add-money-btn").addEventListener("click", function(e){
@@ -27,4 +28,34 @@ document.getElementById("add-money-btn").addEventListener("click", function(e){
     document.getElementById("available-balance").innerText = totalNewAvailableBalance
 
     
+})
+
+// cashout money feature
+
+document.getElementById("withdraw-btn").addEventListener("click", function(e){
+    e.preventDefault()
+
+    const amount = parseInt(document.getElementById("withdraw-amount").value)
+
+    const availableBalance = parseInt(document.getElementById("available-balance").innerText)
+
+    const totalNewAvailableBalance = availableBalance - amount
+    
+    document.getElementById("available-balance").innerText = totalNewAvailableBalance
+})
+
+
+
+
+
+
+//  toggling feature
+
+document.getElementById("add-button").addEventListener("click", function(){
+    document.getElementById("cash-out-parent").style.display = "none"
+    document.getElementById("add-money-parent").style.display = "block"
+})
+document.getElementById("cashout-button").addEventListener("click", function(){
+    document.getElementById("add-money-parent").style.display = "none"
+    document.getElementById("cash-out-parent").style.display = "block"
 })
